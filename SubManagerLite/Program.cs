19ac37f9adfe387a1229319d3a8ba61f1e4a466b
irixos <274342392+irixos@users.noreply.github.com>
2026-04-12 03:54:36 -0400

    add & enable swaggerUI

--- SubManagerLite/Program.cs
+++ SubManagerLite/Program.cs
@@ -10,6 +10,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    app.UseSwaggerUI(options =>
+        options.SwaggerEndpoint("/openapi/v1.json", "v1"));
 }
 
 app.UseHttpsRedirection();
