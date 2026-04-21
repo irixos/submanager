8ad1ade78a1e78c1b35363a605bd9f1b993523b3
irixos <274342392+irixos@users.noreply.github.com>
2026-04-21 02:44:03 -0400

    add ChannelRepository with basic CRUD operations

---
+++ SubManagerLite/Data/Repositories/ChannelRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubManagerLite.Application.Entities;
+
+namespace SubManagerLite.Data.Repositories;
+
+public sealed class ChannelRepository(ApplicationDbContext db)
+{
+    public Task<List<Channel>> GetAllAsync(CancellationToken ct)
+    {
+        return db.Channels.ToListAsync(ct);
+    }
+    
+    public Task AddAsync(Channel channel, CancellationToken ct)
+    {
+        db.Channels.Add(channel);
+        return db.SaveChangesAsync(ct);
+    }
+
+    public Task UpdateAsync(Channel channel, CancellationToken ct)
+    {
+        db.Channels.Update(channel);
+        return db.SaveChangesAsync(ct);
+    }
+
+    public Task DeleteAsync(Channel channel, CancellationToken ct)
+    {
+        db.Channels.Remove(channel);
+        return db.SaveChangesAsync(ct);
+    }
+}
