59031521929f63489ba04ee35df06b6ddb7fcad1
irixos <274342392+irixos@users.noreply.github.com>
2026-05-01 13:18:18 -0400

    update Channel and Video schemas to use SYSUTCDATETIME as default for timestamps and set default value for IsActive

---
+++ SubManagerLite/Infrastructure/Migrations/20260501171329_UpdateChannelAndVideoSchema.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SubManagerLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateChannelAndVideoSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "MetadataLastRefreshedAt",
+                table: "Videos",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSUTCDATETIME()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "SYSDATETIMEOFFSET()");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "AddedDate",
+                table: "Videos",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSUTCDATETIME()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "SYSDATETIMEOFFSET()");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Channels",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "AddedDate",
+                table: "Channels",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSUTCDATETIME()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "SYSDATETIMEOFFSET()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "MetadataLastRefreshedAt",
+                table: "Videos",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSDATETIMEOFFSET()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "SYSUTCDATETIME()");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "AddedDate",
+                table: "Videos",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSDATETIMEOFFSET()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "SYSUTCDATETIME()");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Channels",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "AddedDate",
+                table: "Channels",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSDATETIMEOFFSET()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "SYSUTCDATETIME()");
+        }
+    }
+}
