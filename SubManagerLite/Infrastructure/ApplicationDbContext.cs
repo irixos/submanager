d29913080dba628434517ae624bd3201831229f7
irixos <274342392+irixos@users.noreply.github.com>
2026-05-24 14:35:16 -0400

    add Identity Core entities

--- SubManagerLite/Infrastructure/ApplicationDbContext.cs
+++ SubManagerLite/Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 using System.Reflection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SubManagerLite.Application.Entities;
+using SubManagerLite.Infrastructure.Identity;
 
 namespace SubManagerLite.Infrastructure;
 
-public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Category> Categories => Set<Category>();
     public DbSet<Channel> Channels => Set<Channel>();
@@ -12,6 +14,9 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Call base class implementation to configure Identity entities
+        base.OnModelCreating(modelBuilder);
+        
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
