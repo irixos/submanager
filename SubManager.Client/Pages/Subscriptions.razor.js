b3eb20081a7f00e606358b25a7f77e7fee91214b
irixos <274342392+irixos@users.noreply.github.com>
2026-07-29 18:59:50 -0400

    add subscription grid view

--- SubManager.Client/Pages/Subscriptions.razor.js
+++ SubManager.Client/Pages/Subscriptions.razor.js
@@ -1,9 +1,17 @@
 const SHOW_CATEGORIES_KEY = 'submanager.subscriptions.showCategories';
+const VIEW_MODE_KEY = 'submanager.subscriptions.viewMode';
 
-export function getShowCategories() {
-    return localStorage.getItem(SHOW_CATEGORIES_KEY) === 'true';
+export function getPreferences() {
+    return {
+        showCategories: localStorage.getItem(SHOW_CATEGORIES_KEY) === 'true',
+        gridView: localStorage.getItem(VIEW_MODE_KEY) === 'grid'
+    };
 }
 
 export function setShowCategories(value) {
     localStorage.setItem(SHOW_CATEGORIES_KEY, value);
+}
+
+export function setGridView(value) {
+    localStorage.setItem(VIEW_MODE_KEY, value ? 'grid' : 'list');
 }
--- SubManager.Client/Pages/SubscriptionsInterop.cs
+++ SubManager.Client/Pages/SubscriptionsInterop.cs
@@ -5,8 +5,9 @@ namespace SubManager.Client.Pages;
 public sealed class SubscriptionsInterop(IJSRuntime js) : IAsyncDisposable
 {
     private const string ModulePath = "./Pages/Subscriptions.razor.js";
-    private const string GetShowCategoriesMethod = "getShowCategories";
+    private const string GetPreferencesMethod = "getPreferences";
     private const string SetShowCategoriesMethod = "setShowCategories";
+    private const string SetGridViewMethod = "setGridView";
     private IJSObjectReference? module;
 
     private async ValueTask<IJSObjectReference> GetModuleAsync()
@@ -14,16 +15,22 @@ public sealed class SubscriptionsInterop(IJSRuntime js) : IAsyncDisposable
         return module ??= await js.InvokeAsync<IJSObjectReference>("import", ModulePath);
     }
 
-    public async ValueTask<bool> GetShowCategoriesAsync()
+    public async ValueTask<SubscriptionsPreferences> GetPreferencesAsync()
     {
         var jsModule = await GetModuleAsync();
-        return await jsModule.InvokeAsync<bool>(GetShowCategoriesMethod);
+        return await jsModule.InvokeAsync<SubscriptionsPreferences>(GetPreferencesMethod);
     }
 
     public async ValueTask SetShowCategoriesAsync(bool value)
     {
         var jsModule = await GetModuleAsync();
         await jsModule.InvokeVoidAsync(SetShowCategoriesMethod, value);
+    }
+
+    public async ValueTask SetGridViewAsync(bool value)
+    {
+        var jsModule = await GetModuleAsync();
+        await jsModule.InvokeVoidAsync(SetGridViewMethod, value);
     }
 
     public async ValueTask DisposeAsync()
@@ -37,4 +44,11 @@ public sealed class SubscriptionsInterop(IJSRuntime js) : IAsyncDisposable
         {
         }
     }
+}
+
+public sealed class SubscriptionsPreferences
+{
+    public bool ShowCategories { get; init; }
+
+    public bool GridView { get; init; }
 }
